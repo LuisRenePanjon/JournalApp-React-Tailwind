e6fa1742760866ef6164f00d5c612650f6b7ea58
Luis René Panjón <luchorene97@gmail.com>
2022-03-11 00:06:12 -0500

    Install and configure tailwind

---
+++ src/JournalApp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css'
+export const JournalApp = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold">React with talwind</h1>
+        </div>
+
+    )
+}
