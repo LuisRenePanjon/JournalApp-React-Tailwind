ec7c9297693658d8a70923a5291bb65cf9c0e7f2
Luis René Panjón <luchorene97@gmail.com>
2022-03-31 11:26:24 -0500

    Fix field name

--- src/components/auth/RegisterPage.jsx
+++ src/components/auth/RegisterPage.jsx
@@ -4,7 +4,7 @@ import { AuthForm } from './AuthForm'
 export const RegisterPage = () => {
   const fields = [
     { name: 'name', type: 'text', required: true, title: 'Nombres' },
-    { name: 'name', type: 'text', required: true, title: 'Apellidos' },
+    { name: 'lastName', type: 'text', required: true, title: 'Apellidos' },
     { name: 'email', type: 'email', required: true, title: 'Correo electrónico' },
     { name: 'password', type: 'password', required: true, title: 'Contraseña' }
 
