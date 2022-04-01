38ede6846b0b44e9e65d6d62d9bc9a2cf0323486
Luis René Panjón <luchorene97@gmail.com>
2022-03-31 19:08:16 -0500

    Config Firebase credentials

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,26 @@
+import * as firebase from 'firebase/app';
+import * as auth from 'firebase/auth';
+import * as firestore from 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firestore.getFirestore();
+
+const googleAuthProvider = new auth.GoogleAuthProvider();
+
+
+export { googleAuthProvider, firebase, auth, db };
