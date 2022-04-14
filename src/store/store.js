b928673b9d17e08c68087422ffec2d3c41d06bd1
Luis René Panjón <luchorene97@gmail.com>
2022-04-14 13:41:33 -0500

    Add types for ui and define ui reducer in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,17 @@
-import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import {applyMiddleware, combineReducers, createStore} from 'redux';
 import thunk from 'redux-thunk';
-import { authReducer } from '../reducers/authReducer';
+import {authReducer} from '../reducers/authReducer';
+import {uiReducer} from "../reducers/uiReducer";
+import {composeWithDevTools} from "@redux-devtools/extension";
 
-const composeEnhancers =
-    (typeof window !== 'undefined' &&
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-    compose;
 
 const reducers = combineReducers({
     auth: authReducer,
+    ui: uiReducer,
 });
 
 export const store = createStore(
     reducers,
-    composeEnhancers(applyMiddleware(thunk))
+    composeWithDevTools(applyMiddleware(thunk)),
+    // composeEnhancers(applyMiddleware(thunk))
 );
--- src/types/types.js
+++ src/types/types.js
@@ -1,5 +1,6 @@
-export const types = { 
+export const types = {
     login: '[Auth] Login',
-    logout: '[Auth] Logout'
-
+    logout: '[Auth] Logout',
+    uiSetError: '[UI] Set Error',
+    uiRemoveError: '[UI] Remove Error',
 }
