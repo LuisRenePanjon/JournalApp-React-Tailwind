88fa988b14e10c250eab7f1cd217a3d9e0ec24e3
Luis René Panjón <luchorene97@gmail.com>
2022-04-25 23:39:52 -0500

    Keep auth state when recharge page

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import { useEffect } from 'react'
+
+import { auth } from '../firebase/firebaseConfig';
+
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { AuthLayout } from '../components/auth/AuthLayout'
 // import { LoginPage } from '../components/auth/LoginPage'
 // import { RegisterPage } from '../components/auth/RegisterPage'
 import { JournalPage } from '../components/journal/JournalPage'
+import { useDispatch } from 'react-redux';
+import { login } from '../actions/auth';
 
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        auth.onAuthStateChanged(auth.getAuth(),user => {
+            if (user?.uid) {
+                dispatch(login(user.uid, user.displayName));
+            } else {
+                
+            }
+        });
+    }, [dispatch])
+
+
+
     return (
         <BrowserRouter>
             <Routes>
