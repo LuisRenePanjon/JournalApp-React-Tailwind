ad1fbbf34a29819b9f9b4004f738416f6d8a6ab6
Luis René Panjón <luchorene97@gmail.com>
2022-04-25 23:35:15 -0500

    Add ui types for loading

--- src/types/types.js
+++ src/types/types.js
@@ -1,6 +1,10 @@
 export const types = {
     login: '[Auth] Login',
     logout: '[Auth] Logout',
+
+    uiLoading: '[UI] Loading',
+    uiEndLoading: '[UI] End Loading',
+
     uiSetError: '[UI] Set Error',
     uiRemoveError: '[UI] Remove Error',
 }
