fac1bd088c4ba7bf67f04548219f6becafbc08ae
Luis René Panjón <luchorene97@gmail.com>
2022-05-11 12:09:25 -0500

    Create barrel file to auth components

---
+++ src/components/auth/index.js
@@ -0,0 +1,6 @@
+export * from './AuthForm';
+export * from './AuthForm';
+export * from './AuthLayout';
+export * from './MobileMenuButton';
+export * from './NavLogo';
+export * from './NavOptions';
