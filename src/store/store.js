7ce4f37189a802b0b0ef8c61d784d0d5a890fdfd
Luis René Panjón <luchorene97@gmail.com>
2022-05-13 11:52:10 -0500

    Add notes reducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,13 @@
 import {applyMiddleware, combineReducers, createStore} from 'redux';
 import thunk from 'redux-thunk';
-import {authReducer} from '../reducers/authReducer';
-import {uiReducer} from "../reducers/uiReducer";
 import {composeWithDevTools} from "@redux-devtools/extension";
+import {authReducer, notesReducer, uiReducer} from "../reducers";
 
 
 const reducers = combineReducers({
     auth: authReducer,
     ui: uiReducer,
+    notes: notesReducer,
 });
 
 export const store = createStore(
