1e6b2de1e4f8c1ffd43ce43d65af86aa435ad567
Luis René Panjón <luchorene97@gmail.com>
2022-05-17 09:28:50 -0500

    Add dispatch to activee note

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,17 +1,25 @@
-
-import {db} from "../firebase/firebaseConfig";
-import {collection, addDoc} from 'firebase/firestore'
-
+import { db } from '../firebase/firebaseConfig';
+import { collection, addDoc } from 'firebase/firestore';
+import { types } from '../types/types';
 
 export const startNewNote = () => {
-	return async (dispatch, getState) => {
-		const uid = getState().auth.uid;
-		const newNote = {
-			title: '',
-			body: '',
-			date: new Date().getTime(),
-		}
-		const collRef = collection(db, uid);
-		await addDoc(collection(collRef, 'journal', 'notes'), newNote);
-	}
+    return async (dispatch, getState) => {
+        const uid = getState().auth.uid;
+        const newNote = {
+            title: '',
+            body: '',
+            date: new Date().getTime(),
+        };
+        const collRef = collection(db, uid);
+        const noteRef = await addDoc(collection(collRef, 'journal', 'notes'), newNote);
+		dispatch(activeNote(noteRef.id, newNote));
+	};
 };
+
+export const activeNote = (id, note) => ({
+    type: types.notesActive,
+    payload: {
+        id,
+        ...note,
+    },
+});
