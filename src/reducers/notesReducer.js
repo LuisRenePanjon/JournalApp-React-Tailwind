39ceec3d78ad199812fff15a5351d2cf1efcf73a
Luis René Panjón <luchorene97@gmail.com>
2022-05-17 09:29:21 -0500

    Add reducer to active note

--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -1,26 +1,33 @@
+import { types } from '../types/types';
+
 const initialState = {
-	notes: [],
-	active: null,
-}
+    notes: [],
+    active: null,
+};
 
 export const notesReducer = (state = initialState, action) => {
-  switch (action.type) {
-	case 'ADD_NOTE':
-	  return [...state, action.payload];
-	case 'DELETE_NOTE':
-	  return state.filter(note => note.id !== action.payload);
-	case 'EDIT_NOTE':
-	  return state.map(note => {
-		if (note.id === action.payload.id) {
-		  return {
-			...note,
-			...action.payload.updates
-		  };
-		} else {
-		  return note;
-		}
-	  });
-	default:
-	  return state;
-  }
+    switch (action.type) {
+        case types.notesActive:
+            return {
+                ...state,
+                active: {
+                    ...action.payload,
+                },
+            };
+        case 'DELETE_NOTE':
+            return state.filter((note) => note.id !== action.payload);
+        case 'EDIT_NOTE':
+            return state.map((note) => {
+                if (note.id === action.payload.id) {
+                    return {
+                        ...note,
+                        ...action.payload.updates,
+                    };
+                } else {
+                    return note;
+                }
+            });
+        default:
+            return state;
+    }
 };
