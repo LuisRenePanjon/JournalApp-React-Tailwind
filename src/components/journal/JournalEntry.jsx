fa59b2fda9c1b0f6d280991ffa84ecb6618cf756
Luis René Panjón <luchorene97@gmail.com>
2022-05-18 20:12:31 -0500

    Show entry with real data

--- src/components/journal/JournalEntry.jsx
+++ src/components/journal/JournalEntry.jsx
@@ -1,19 +1,35 @@
+import { useDispatch } from "react-redux";
+import { activeNote } from "../../actions/notes";
+import { miliToCustomDate } from "../../adapters/miliToCustomDate"
 
-export const JournalEntry = () => {
+export const JournalEntry = ({ id, title, body, date, url }) => {
+    const { day, weekday } = miliToCustomDate(date);
 
+    const dispatch  = useDispatch()
 
+    const handleEntryClick = () => {
+        dispatch(activeNote(id, {
+            date, title, body, url
+        }))   
+    }
     return (
-        <div >
-            <div className="bg-white rounded-md shadow-md p-5 flex flex-row text-black justify-between m-5 ">
+        <div onClick={handleEntryClick}>
+            <div className="bg-white rounded-md shadow-md p-5 flex flex-row text-black justify-between m-5 cursor-pointer">
+                {url &&
+                    <div style={{
+                        backgroundImage: `url(${url})`,
+                        backgroundSize: 'cover',
+
+                    }} />
+                }
+
                 <div>
-                    img
+                    <h1>{title}</h1>
+                    <h3>{body}</h3>
                 </div>
-                <div>
-                    <h1>title</h1>
-                    <h3>description</h3>
-                </div>
-                <div>
-                    date
+                <div className="flex justify-center flex-col items-center  h-full w-20">
+                    <h1 className="font-medium">{weekday}</h1>
+                    <h1 className="font-bold text-xl">{day}</h1>
                 </div>
             </div>
         </div>
