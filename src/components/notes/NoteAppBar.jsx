61be711dcd8304ef0edee12468c33025c832ec9c
Luis René Panjón <luchorene97@gmail.com>
2022-05-24 12:57:06 -0500

    remove delete button

--- src/components/notes/NoteAppBar.jsx
+++ src/components/notes/NoteAppBar.jsx
@@ -2,18 +2,14 @@ import { SideBar } from '../journal'
 
 export const NoteAppBar = () => {
 
-    const handleDeleteClick = () => {
-        console.log('Delete Clicked')
-    }
+
 
     return (
         <div className="items-center bg-gray-900 text-white flex justify-between px-5 py-4 ">
             <SideBar/>
             <span>28 de marzo 2022</span>
             <div>
-                <button  onClick={handleDeleteClick} className='mx-2 hover:bg-red-300 transition-all p-3 bg-red-600  rounded-lg'>
-                    Borrar
-                </button>
+
             </div>
         </div>
     )
